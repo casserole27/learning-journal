2594b03d24e652fc8cc461c31380a7ab787805b1
C Lewis <casserolecodes@gmail.com>
2023-05-20 19:55:22 -0400

    JS logic for hamburger menu

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 /****** VARIABLES ******/
 
-
-
+const navMenu =  document.getElementById('nav-menu');
+const navBtn = document.getElementById('nav-btn');
 
 
 /****** FUNCTIONS ******/
 
 
+function showNavMenu() {
+    // if (e.target.id === 'BUTTON') {
+    navMenu.classList.toggle('hidden');
+    navBtn.classList.toggle('active-btn');
+    console.log('button clicked')    
+// };
+};
 
+const navMenuToggle = () => {
+    console.log("nav menu clicked!")
+
+}
 
 
 /****** EVENT LISTENERS ******/
 
-document.getElementById('test-btn').addEventListener('click', () => {
-    document.getElementById('render-test').innerHTML = `
-    <p>JS render test complete</p>`
-})
+navBtn.addEventListener('click', showNavMenu);
+// document.addEventListener('click', showNavMenu);
