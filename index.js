f8a6bbfac7b32bb4dcdbd6ab829ee45258f49dda
C Lewis <casserolecodes@gmail.com>
2023-05-20 21:21:17 -0400

    finished JS logic for view more posts

--- index.js
+++ index.js
@@ -2,26 +2,29 @@
 
 const navMenu =  document.getElementById('nav-menu');
 const navBtn = document.getElementById('nav-btn');
+const viewBtn = document.getElementById('view-btn');
 
 
 /****** FUNCTIONS ******/
 
 
 function showNavMenu() {
-    // if (e.target.id === 'BUTTON') {
     navMenu.classList.toggle('hidden');
     navBtn.classList.toggle('active-btn');
     console.log('button clicked')    
-// };
 };
 
-const navMenuToggle = () => {
-    console.log("nav menu clicked!")
+function showMorePosts() {
+    const posts = document.querySelectorAll('article');
+    posts.forEach((post) => {
+        post.classList.remove('hidden-post')
+    })
 
-}
+    viewBtn.classList.add('hidden-btn')
+};
 
 
 /****** EVENT LISTENERS ******/
 
 navBtn.addEventListener('click', showNavMenu);
-// document.addEventListener('click', showNavMenu);
+viewBtn.addEventListener('click', showMorePosts)
