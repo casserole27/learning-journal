5cf15f817f81af10dea5c93b96558ce47b4f586c
C Lewis <casserolecodes@gmail.com>
2023-06-11 08:45:15 -0400

    added JS video data and rendering functions

---
+++ data.js
@@ -0,0 +1,129 @@
+const recentVideoData = [
+    // invoice: {
+        {    
+            video: "assets/videos/invoice-creator.mp4",
+            date: "May 2023",
+            title: "Invoice Creator",
+            description: `This was a great exercise in manipulating user input data, 
+            converting it to an array of objects, 
+            and then rendering and manipulating that data. 
+            I also added some UX enhancements to direct users 
+            if they enter an invalid entry such as blank inputs, 
+            duplicate tasks, and entering tasks without a price.`
+        },
+    
+        // vader: {
+        {
+            video: "assets/videos/Star Wars Day.mp4",
+            date: "May 2023",
+            title: "Darth Vader Quote Gnerator",
+            description: `
+            I love movies. I especially love science fiction movies. When I was a kid, 
+            I wanted to be Luke Skywalker. When I grew up a little, I dreamed of working 
+            for Industrial Light and Magic creating special effects for movies. With this first attempt at CSS art, 
+            I can be a small part of that magic.
+            `
+        },
+    
+        // starrating: {
+        {    
+            video: "assets/videos/star-rating.mp4",
+            date: "April 2023",
+            title: "Star Rating",
+            description: `
+            For the group project I'm working on, 
+            I made a clickable star rating using HTML, CSS, and vanilla JS. 
+            I also started studying how to make the usage of icons more accessible.
+            `
+        },
+
+]
+
+const videoData = [
+
+
+    // flexwrap: {
+    {    
+        video: "assets/videos/flex-wrap.mp4",
+        date: "April 2023",
+        title: "Flex-Wrap Image Grid",
+        description: `
+        Flex-wrap and flex-basis/grow/shrink and the flex shorthand property were all new to me. 
+        When I saw how to use these properties to make a flexbox image grid, 
+        I had to try it immediately! So, I made this small spring image gallery.
+        `
+    },
+
+    // marchmadness: {
+    {
+        video: "assets/videos/march-madness.mp4",
+        date: "March 2023",
+        title: "March Madness Challenge",
+        description: `
+        💣 Embracing failure : while I did not bomb my challenge, 
+        I did a whole lot of failing at JavaScript. 
+        My biggest takeaway from this is to set time limits on problems, 
+        step away from them and come back, and then ask for help. 
+        At one point I was able to solve an ongoing problem in five minutes 
+        just by giving it a day-long break!
+        `
+    },
+
+    // unitconverter: {
+    {    
+        video: "assets/videos/unit-converter.mp4",
+        date: "February 2023",
+        title: "Unit Converter",
+        description: `
+        I learned more about HTML number inputs, and UX / accessibility is enhanced by 
+        CSS pseudo-classes :hover, :focus, and :active. I was able to refactor the 
+        JS get to get rid of some repetitive lines and make it DRYer. I also presented this project 
+        in a bootcamp session to get more comfortable talking about code.
+        `
+    },
+
+    // catan: {
+    {    
+        video: "assets/videos/catan-scorekeeper.mp4",
+        date: "February 2023",
+        title: "Settlers of Catan Scorekeeper",
+        description: `
+        I created this UI and color palette, 
+        and I added some hover states to highlight the buttons. 
+        It is nicely responsive between desktop and mobile. 
+        I completed a small dropdown menu in JS to hide the scoring rules 
+        because I wanted this to be as compact as possible on different screen sizes.
+        `
+    },
+
+    // password: {
+    {    
+        video: "assets/videos/password-generator.mp4",
+        date: "January 2023",
+        title: "Password Generator",
+        description: `
+        By completing the stretch goals on this project,
+        I learned a TON about input fields and forms, and how they work with 
+        HTML, CSS, and Javascript combined. And... it works! One of the things 
+        I love about the Scrimba bootcamp is we get code reviews, 
+        so I'm very interested to see how this turned out, get my questions answered, 
+        and see how I can make it better.
+        `
+    },
+
+    // portfolio: {
+    {    
+        video: "assets/videos/portfolio-walkthru.mp4",
+        date: "January 2023 - present",
+        title: "Portfolio Evolution Walk-Thru",
+        description: `
+        This is made with UI design, HTML, CSS, JavaScript, and I'm sure I'll want to change
+        it again once I get further into JavaScript and React. It's designed 
+        to grow with the Scrimba frontend developer career path 
+        as I move through the modules.
+        `
+    }
+
+];
+
+export { recentVideoData, videoData };
