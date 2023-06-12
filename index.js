f774f0b3207f5845a21fec7960fdfa18d6374961
C Lewis <casserolecodes@gmail.com>
2023-06-11 22:16:08 -0400

    added accordion menu via HTML

--- index.js
+++ index.js
@@ -54,22 +54,6 @@ function showNavMenu() {
     navBtn.classList.toggle('active-btn');
 };
 
-// const acc = document.getElementsByClassName('accordion');
-
-// for (let i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener('click', () => {
-//         // this.classList.toggle('active');
-
-//         let panel = this.nextElementSibling;
-//         if (panel.style.display === "block") {
-//           panel.style.display = "none";
-//         } else {
-//           panel.style.display = "block";
-//         }
-//     });
-// };
-
-
 /****** EVENT LISTENERS ******/
 
 navBtn.addEventListener('click', showNavMenu);
